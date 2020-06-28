19b288888ae7012feff3ba3168cd5df269c96d4a
Septian Solihin <septian.solihin@btpn.com>
2020-06-28 17:08:44 +0700

    "initial commit"

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  describe('#render', () => {
+    it('should render welcome', () => {
+      const wrapper = shallow(<App />);
+      const text = wrapper.find('h1');
+
+      expect(text.text()).toEqual('Welcome');
+    });
+  });
 });
